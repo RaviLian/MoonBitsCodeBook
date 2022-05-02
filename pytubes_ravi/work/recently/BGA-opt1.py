bca500baca6299cc52bcde02ef6a233432b02cad
Ravi_Lian <731081939@qq.com>
2022-05-02 21:40:34 +0800

    no use

--- pytubes_ravi/work/recently/BGA-opt1.py
+++ pytubes_ravi/work/recently/BGA-opt1.py
@@ -15,10 +15,10 @@ cost_time_lookup = [
     3,  # 0体质测试   4
     3,  # 1内科      4
     4,  # 2外科      3
-    2,  # 3眼耳口鼻科 5
-    3,  # 4验血      4
-    2,  # 5心电图    5
-    5,  # 6X光      3
+    # 2,  # 3眼耳口鼻科 5
+    # 3,  # 4验血      4
+    # 2,  # 5心电图    5
+    # 5,  # 6X光      3
     6,  # 7B超      2
 ]  # 共28分钟
 
@@ -26,15 +26,15 @@ cost_time_lookup = [
 #     3, 3, 4, 6
 # ]
 
-total_people = 9
+total_people = 12
 project_num = len(cost_time_lookup)
 T_W = 15  # 等待阈值
 GAMMA = 5 # 惩罚系数
-POP_SIZE = 10  # 种群大小
+POP_SIZE = 150  # 种群大小
 GROUP = 30  # 选择权重，百分之百
 CROSS_RATE = 0.8
 MUTATE_RATE = 1.0
-N_GENERATIONS = 5
+N_GENERATIONS = 200
 
 def translate_operation(opt):
     """解码操作"""
